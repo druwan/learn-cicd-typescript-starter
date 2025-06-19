77fc88f2afd05ce1b8ffe1f3facb62f8054e2d68
Christopher Vestman <christophervestman@gmail.com>
2025-06-19 23:09:11 +0200

    CH02 L01

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "src/api/auth";
+
+
+
+
+describe('getAPIKey', () => {
+  test('APIKey does not exist', () => {
+    const headers = {}
+    const result = getAPIKey(headers)
+    expect(result).toBeNull();
+  });
+})
+
